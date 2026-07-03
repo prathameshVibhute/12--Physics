e3f61e7328a7cf7004cfef62602272f6fdd6746b
prathameshVibhute <prathuvibhute3110@gmail.com>
2026-07-03 08:34:14 +0530

    Removed some files

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,37 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+interface SIZE {
+  width: number,
+  height: number
+}
 
-<div class="ticks"></div>
+const canvas: HTMLElement | null = document.getElementById('three-js');
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({canvas} as any);
+const scene: THREE.Scene = new THREE.Scene();
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+function getSize(): SIZE {
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// Camera
+let size: SIZE = getSize();
+const fieldOfView: number = 45;
+const aspectRatio: number = size.width / size.height;
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(fieldOfView,aspectRatio);
+camera.position.z = 10;
+scene.add(camera);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+window.addEventListener('resize',() => {
+  size = getSize();
+  camera.aspect = size.width / size.height;
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(size.width,size.height);
+  renderer.render(scene,camera);
+});
+
+// Renderer
+renderer.setSize(size.width,size.height);
+renderer.render(scene,camera);
